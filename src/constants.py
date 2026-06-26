d51859c0636ae2f709c414e518ab9a6ce23e8cb2
nolvyn <245221879+nolvyn@users.noreply.github.com>
2026-06-26 11:00:27 -0700

    Parallelized waveform generation using joblib

--- src/constants.py
+++ src/constants.py
@@ -3,7 +3,7 @@ from pycbc.waveform import td_approximants, fd_approximants
 
 # If the number 1 is seen here, that is simply for quick testing purposes
 # 100 - 1000 typically provide good results
-DRAW_QUANTITY = 10
+DRAW_QUANTITY = 100
 OAT_RESOLUTION = 20
 
 THRESH_NUM = 0.0001
--- src/main.py
+++ src/main.py
@@ -1,25 +1,38 @@
 import warnings
-import os
-import sys
-
-import constants as C
-import gwtc
-import utils
 
 warnings.filterwarnings("ignore", "Wswiglal-redir-stdio")
 warnings.filterwarnings("ignore", "pkg_resources is deprecated as an API")
 
+import os
+import sys
+
+for var in (
+    "OMP_NUM_THREADS",
+    "OPENBLAS_NUM_THREADS",
+    "MKL_NUM_THREADS",
+    "NUMEXPR_NUM_THREADS",
+):
+    os.environ.setdefault(var, "1")
+
+import numpy as np
+from joblib import Parallel, delayed
+
+import constants as C
+import gwtc
+import lal
+import utils
+
+lal.swig_redirect_standard_output_error(False)
 
 sys.path.append(os.path.abspath(os.path.join(os.getcwd(), "src")))
 
 # Set up proper LAL Data Path (Needed for accessing SEOBNR and NR files)
 os.environ["LAL_DATA_PATH"] = os.path.expanduser("~/projects/gw-analysis/data/models")
 
-# Main Stuff
-waveform_data = {}
 
-for event in gwtc.ALL_EVENTS:
+def process_event(event):
     samples = utils.get_parameters(event)
+    waveform_data = {}
 
     for wfm_a, wfm_b in C.MODEL_PAIRS:
         label = f"{event} {wfm_a} vs {wfm_b}"
@@ -49,18 +62,34 @@ for event in gwtc.ALL_EVENTS:
                     "spin2z": spin2z,
                     "distance": distance,
                     "inclination": inclination,
-                    "freqs": characteristics["freqs"],
-                    "amp": characteristics["amp"],
-                    "d_A": characteristics["d_A"],
-                    "d_phi": characteristics["d_phi"],
-                    "d_phi_R": characteristics["d_phi_R"],
-                    "freqs_dimless": characteristics["freqs_dimless"],
+                    "freqs": np.asarray(characteristics["freqs"], dtype=np.float32),
+                    "amp": np.asarray(characteristics["amp"], dtype=np.float32),
+                    "d_A": np.asarray(characteristics["d_A"], dtype=np.float32),
+                    "d_phi": np.asarray(characteristics["d_phi"], dtype=np.float32),
+                    "d_phi_R": np.asarray(characteristics["d_phi_R"], dtype=np.float32),
+                    "freqs_dimless": np.asarray(
+                        characteristics["freqs_dimless"], dtype=np.float32
+                    ),
                     "mass_ratio": mass_ratio,
                     "total_mass": total_mass,
                     "chi_eff": chi_eff,
                     "mismatch": characteristics["mismatch"],
                 }
             )
+
+    return waveform_data
+
+
+sample = utils.get_parameters(gwtc.ALL_EVENTS[0])[0]
+for wfm_a, wfm_b in C.MODEL_PAIRS:
+    utils.generate_waveform(sample, wfm_a, wfm_b)
+
+waveform_data = {}
+
+for result in Parallel(n_jobs=16, backend="multiprocessing")(
+    delayed(process_event)(event) for event in gwtc.ALL_EVENTS
+):
+    waveform_data.update(result)
 
 if C.RUN_SPREAD_PLOTS:
     import spread_plots
@@ -77,14 +106,15 @@ if C.RUN_MISMATCH_PLOTS:
 
     mismatch_plots.run(waveform_data)
 
+if C.RUN_VARY_PLOTS or C.RUN_OAT_PLOTS:
+    percentiles = utils.collect_percentiles(gwtc.ALL_EVENTS)
+
 if C.RUN_VARY_PLOTS:
     import vary_param_plots
 
-    percentiles = utils.collect_percentiles(gwtc.ALL_EVENTS)
     vary_param_plots.run(percentiles)
 
 if C.RUN_OAT_PLOTS:
     import oat_plots
 
-    percentiles = utils.collect_percentiles(gwtc.ALL_EVENTS)
     oat_plots.run(percentiles)
